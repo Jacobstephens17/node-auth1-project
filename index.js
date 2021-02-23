5eca0497fd297429d89e0add439d8c39372b759d
Jacob Stephens <jacobstephens2@gmail.com>
2021-02-22 16:54:36 -0800

    changed port back to .env port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config
 const server = require('./api/server.js')
 
-// const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000
 
-server.listen(4000, () => {
-    console.log(`Server listening on ${4000}`)
+server.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`)
 })
